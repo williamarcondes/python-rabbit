ce3544e3fc5bd4d4e8fbca2b685c544e39ce63c2
Will <williamarcondes@betrybe.com>
2022-05-05 12:34:51 -0300

    RabbitMq + Python

--- send.py
+++ send.py
@@ -9,6 +9,6 @@ channel = connection.channel()
 
 channel.queue_declare(queue="hello")
 
-channel.basic_publish(exchange="", routing_key="hello", body="Hello World!")
-print(" [x] Envia 'Hello World!'")
+channel.basic_publish(exchange="", routing_key="hello", body="Ola Juliana World!")
+print(" [x] Envia 'Oi Juliana'")
 connection.close()
