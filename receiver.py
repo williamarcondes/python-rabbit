9b4255c203c32b3de643d6668c9e105e5984e90d
Will <williamarcondes@betrybe.com>
2022-05-09 03:47:20 -0300

    Simula raspagem

--- receiver.py
+++ receiver.py
@@ -1,20 +1,22 @@
 import pika, os
-
+import time
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host=os.environ.get("RABBITMQ_HOST"))
 )
 channel = connection.channel()
 
-channel.queue_declare(queue="hello")
+channel.queue_declare(queue="urls")
 
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    print(f"Raspando o conteúdo da URL = https://www.tecmundo.com.br/{body}")
+    time.sleep(30)
+    print("[x] Conteúdo Raspado com Sucesso")
 
 
 channel.basic_consume(
-    queue="hello", on_message_callback=callback, auto_ack=True
+    queue="urls", on_message_callback=callback, auto_ack=True
 )
 
 print(" [*] Waiting for messages. To exit press CTRL+C")
--- send.py
+++ send.py
@@ -7,8 +7,28 @@ connection = pika.BlockingConnection(
 )
 channel = connection.channel()
 
-channel.queue_declare(queue="hello")
+channel.queue_declare(queue="urls")
 
-channel.basic_publish(exchange="", routing_key="hello", body="Ola Juliana World!")
-print(" [x] Envia 'Oi Juliana'")
+
+urls = [
+    "minha-serie/238164-ordem-cronologica-filmes-series-marvel-confira.htm",
+    "voxel/238069-minecraft-criar-servidor-game.htm",
+    "mercado/238200-apple-nao-aceita-cartoes-credito-debito-india.htm",
+    "internet/238197-mailtrack-usar-gmail.htm",
+    "internet/238079-gmail-veja-cancelar-envio-mail.htm",
+    "produto/238199-veja-orimeiros-cabos-usb-c-padrao-2-1-240w.htm",
+    "mercado/237981-pesquisador-ia-demitido-desafiar-google.htm",
+    "cultura-geek/238193-sam-raimi-doutor-estranho-2-nao-primeiro-filme-heroi-veja.htm",
+    "voxel/236534-melhores-jogos-coop-jogar-amigos-pc.htm",
+    "software/238198-whatsapp-usar-novas-reacoes-mensagens.htm",
+    "internet/238197-mailtrack-usar-gmail.htm",
+    "minha-serie/238196-doctor-who-ator-sex-education-novo-protagonista.htm",
+]
+
+
+for url in urls:
+    print(f" [x] {url}")
+    channel.basic_publish(exchange="", routing_key="urls", body=url)
+
+print(" [x] Concluído envio de todas URLS")
 connection.close()
